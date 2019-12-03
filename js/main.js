5f4524a5fdbb482ba99b0d0d57b50e3823d3e516
Roberto Yllescas <li.yllescas@gmail.com>
2019-12-02 21:19:05 -0800

    fist commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+var selectedRow = null
+
+function onFormSubmit() {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+}
+
+function readFormData() {
+    var formData = {};
+    formData["firstName"] = document.getElementById("firstName").value;
+    formData["lastName"] = document.getElementById("lastName").value;
+    formData["email"] = document.getElementById("email").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("contactList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.firstName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.lastName;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.email;
+    cell3 = newRow.insertCell(3);
+    cell3.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("firstName").value = "";
+    document.getElementById("lastName").value = "";
+    document.getElementById("email").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("firstName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("lastName").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[2].innerHTML;
+    
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.firstName;
+    selectedRow.cells[1].innerHTML = formData.lastName;
+    selectedRow.cells[2].innerHTML = formData.email;
+    
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("contactList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
